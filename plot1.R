6632f5c51e36fabb887be1fd624a457bd39921f3
Malik Coleman <flybyday@gmail.com>
2014-06-08 12:37:20 -0700

    Updated R code

--- plot1.R
+++ plot1.R
@@ -1,4 +1,6 @@
 hpc<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
 hpc$Date<-as.Date(hpc$Date, "%d/%m/%Y")
 pwrdata<-subset(hpc,Date=="2007-02-01"|Date=="2007-02-02")
+png(file="plot1.png")
 hist(pwrdata$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
+dev.off()
--- plot2.R
+++ plot2.R
@@ -3,4 +3,6 @@ hpc$Date<-as.Date(hpc$Date, "%d/%m/%Y")
 pwrdata<-subset(hpc,Date=="2007-02-01"|Date=="2007-02-02")
 pwr2<-as.POSIXct(strptime(paste(pwrdata$Date,pwrdata$Time),"%Y-%m-%d %H:%M:%S"))
 pwrdata$Date<-pwr2
+png(file="plot2.png")
 plot(pwrdata$Date,pwrdata$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=NA)
+dev.off()
